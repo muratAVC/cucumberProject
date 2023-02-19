ccf56ec67a962e0a0ac47c5d3f8181af4531eedd
muratAVC <muratriyum@gmail.com>
2023-02-19 22:16:07 +0300

    day14. cucumber project HTML report dryRun Plagin Tag

---
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -0,0 +1,39 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_StepDefinitions {
+    @When("user enters librarian username")
+    public void user_enters_librarian_username() {
+        System.out.println("user enters librarian username");
+    }
+    @When("user enters librarian password")
+    public void user_enters_librarian_password() {
+        System.out.println("user enters librarian password");
+    }
+    @Then("user should see the dashboard")
+    public void user_should_see_the_dashboard() {
+        System.out.println("user enters librarian dashbord");
+    }
+
+    @When("user enters student username")
+    public void user_enters_student_username() {
+        System.out.println("user enters student username");
+    }
+    @When("user enters student password")
+    public void user_enters_student_password() {
+        System.out.println("user enters student pass");
+    }
+
+    @When("user enters admin username")
+    public void userEntersAdminUsername() {
+        System.out.println("user enters admin username");
+    }
+
+    @And("user enters admin password")
+    public void userEntersAdminPassword() {
+        System.out.println("user enters admin password");
+    }
+}
