29628afdd37b891d3ba99740eda8332777fbc9af
muratAVC <muratriyum@gmail.com>
2023-02-19 18:31:43 +0300

    last succesfuly cucumber project creations

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,9 +1,32 @@
 package com.cydeo.step_definitions;
 
-public class GoogleStepDefinitions {
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
 
-    public void deneGit(){
-        System.out.println("selam nasılsın git");
+public class GoogleStepDefinitions {
+    WebDriver driver;
+    @Before
+    public void startDriver(){
+        driver=Driver.getDriver(Driver.CHROME);
+    }
+    @After
+    public void closeDriver(){
+        driver.quit();
     }
 
+
+    @When("user is on google search page")
+    public void user_is_on_google_search_page() {
+        driver.get("https://www.google.com");
+    }
+    @Then("user should see title is Google")
+    public void user_should_see_title_is_google() {
+        Assert.assertTrue(driver.getTitle().contains("Google"));
+
+    }
 }
--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -2,9 +2,7 @@ package com.cydeo.utilities;
 
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-
+import java.util.Set;
 
 public class BrowserUtils {
     public WebDriver driver;
@@ -12,13 +10,17 @@ public class BrowserUtils {
     public void verifyTitle(WebDriver driver,String actualTitle){
         Assert.assertTrue(driver.getTitle().equals(actualTitle));
     }
-    @BeforeMethod
-    public void driverStart(){
-        driver= Driver.getDriver(Driver.CHROME);
-    }
-    @AfterMethod
-    public void driverQuit() throws InterruptedException {
-        //Thread.sleep(3000);
-        driver.quit();
+
+    public static void switchWindowAndVerify(String expectedURL, String expectedTitle){
+        Set<String> allWindowsHandles=Driver.getDriver(Driver.CHROME).getWindowHandles();
+
+        for (String vinH:allWindowsHandles) {
+            Driver.getDriver(Driver.CHROME).switchTo().window(vinH);
+            System.out.println(Driver.getDriver(Driver.CHROME).getCurrentUrl());
+            if (Driver.getDriver(Driver.CHROME).getCurrentUrl().contains(expectedURL)){
+                break;
+            }
+        }
+        Assert.assertTrue(Driver.getDriver(Driver.CHROME).getTitle().contains(expectedTitle));
     }
 }
